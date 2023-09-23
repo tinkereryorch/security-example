b15ac3bbb4c2b3a2f2dde45420dcd4c7f0f0c3ed
Jorge Garcia <jegcelorio@yahoo.com>
2023-09-23 09:32:46 -0500

    Added secret path

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/secret', (req, res) => {
+    return res.send('Your personal secret value is PARK');
+});
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
 })
